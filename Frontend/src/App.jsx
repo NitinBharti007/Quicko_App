6e25b5eb02ec4e85bdfce0699d873065ec2fa259
Nitin Bharti <jacksp198@gmail.com>
2024-12-30 10:39:07 +0530

    Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <main>
-      Hii lets start!
-    </main>
-  )
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
