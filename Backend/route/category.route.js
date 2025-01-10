a236e38cefd2f374a7dba5da88d06df6c28c3283
Nitin Bharti <jacksp198@gmail.com>
2025-01-10 16:37:23 +0530

    New

--- Backend/route/category.route.js
+++ Backend/route/category.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddCategoryController } from "../controllers/category.controller.js";
+import {
+  AddCategoryController,
+  GetCategoryController,
+} from "../controllers/category.controller.js";
 
 const categoryRouter = Router();
 
 categoryRouter.post("/add-category", auth, AddCategoryController);
+categoryRouter.get("/get-category", GetCategoryController);
 
 export default categoryRouter;
