6e4e8d142666c11a5d138329f947b0a873fbe309
Nitin Bharti <jacksp198@gmail.com>
2025-01-14 00:58:47 +0530

    Sub

--- Backend/controllers/category.controller.js
+++ Backend/controllers/category.controller.js
@@ -1,4 +1,6 @@
 import CategoryModel from "../models/category.model.js";
+import ProductModel from "../models/product.model.js";
+import SubCategoryModel from "../models/subCategory.model.js";
 
 export const AddCategoryController = async (req, res) => {
   try {
@@ -57,9 +59,11 @@ export const GetCategoryController = async (req, res) => {
 export const UpdateCategoryController = async (req, res) => {
   try {
     const { _id, name, image } = req.body;
-    const update = await CategoryModel.updateOne({
+    const update = await CategoryModel.updateOne(
+      {
         _id: _id,
-      },{
+      },
+      {
         name,
         image,
       }
@@ -69,6 +73,42 @@ export const UpdateCategoryController = async (req, res) => {
       data: update,
       error: false,
       success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const DeleteCategoryController = async (req, res) => {
+  try {
+    const { _id } = req.body;
+    const checkSubCategory = await SubCategoryModel.find({
+      category: {
+        "$in": [_id],
+      },
+    }).countDocuments();
+    const checkProduct = await ProductModel.find({
+      category: {
+        "$in": [_id],
+      },
+    }).countDocuments();
+    if (checkSubCategory > 0 || checkProduct > 0) {
+      return res.status(400).json({
+        message: "Category has sub category, can't delete",
+        error: true,
+        success: false,
+      });
+    }
+    const deleteCategory = await CategoryModel.deleteOne({ _id: _id });
+    return res.json({
+      message: "Category Deleted",
+      error: false,
+      success: true,
+      data: deleteCategory,
     });
   } catch (error) {
     return res.status(500).json({
--- Backend/route/category.route.js
+++ Backend/route/category.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import auth from "../middleware/auth.js";
 import {
   AddCategoryController,
+  DeleteCategoryController,
   GetCategoryController,
   UpdateCategoryController,
 } from "../controllers/category.controller.js";
@@ -11,5 +12,6 @@ const categoryRouter = Router();
 categoryRouter.post("/add-category", auth, AddCategoryController);
 categoryRouter.get("/get-category", GetCategoryController);
 categoryRouter.put("/update-category", auth, UpdateCategoryController);
+categoryRouter.delete("/delete-category", auth, DeleteCategoryController);
 
 export default categoryRouter;
