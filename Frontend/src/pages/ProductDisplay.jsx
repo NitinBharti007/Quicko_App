9eefc7a9a62a31a3d453060fb1bc0be761f501a5
Nitin Bharti <jacksp198@gmail.com>
2025-03-25 17:32:13 +0530

    Description

--- Frontend/src/pages/ProductDisplay.jsx
+++ Frontend/src/pages/ProductDisplay.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import AxiosToastError from "../utils/AxiosToastError";
 import Axios from "../utils/Axios";
 import SummaryApi from "../common/SummaryApi";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { DisplayPriceInRUpees } from "../utils/DisplayPriceInRupees";
+import Divider from "../components/Divider";
 
 const ProductDisplay = () => {
   const params = useParams();
+  const imageContainer = useRef();
   let productID = params?.product?.split("-")?.slice(-1)[0];
   const [data, setData] = useState({
     name: "",
@@ -37,6 +41,13 @@ const ProductDisplay = () => {
     fetchProdutsDetails();
   }, [params]);
 
+  const handleScollRight = () => {
+    imageContainer.current.scrollLeft += 100;
+  };
+  const handleScollleft = () => {
+    imageContainer.current.scrollLeft -= 100;
+  };
+
   return (
     <section className="container mx-auto p-4 grid lg:grid-cols-2">
       <div className="">
@@ -59,8 +70,11 @@ const ProductDisplay = () => {
             );
           })}
         </div>
-        <div className="grid">
-          <div className="flex gap-4 w-full overflow-x-auto scrollbar-none">
+        <div className="grid relative">
+          <div
+            ref={imageContainer}
+            className="flex gap-4 z-10 relative w-full overflow-x-auto scrollbar-none"
+          >
             {data.image.map((img, index) => {
               return (
                 <div
@@ -77,7 +91,36 @@ const ProductDisplay = () => {
               );
             })}
           </div>
+          <div className="w-full -ml-3 h-full hidden lg:flex justify-between absolute items-center">
+            <button
+              onClick={handleScollleft}
+              className="z-10 bg-white relative p-1 rounded-full shadow-lg"
+            >
+              <FaAngleLeft />
+            </button>
+            <button
+              onClick={handleScollRight}
+              className="z-10 bg-white relative p-1 rounded-full"
+            >
+              <FaAngleRight />
+            </button>
+          </div>
         </div>
+      </div>
+
+      <div className="p-4 text-base lg:text-lg">
+        <p className="bg-green-300 w-fit px-2 rounded-full">10 min</p>
+        <h2 className="text-lg font-semibold lg:text-2xl">{data.name}</h2>
+        <p>{data.unit}</p>
+        <Divider/>
+        <div>
+          <p>Price</p>
+          <div className="border border-green-500 px-3 py-2 bg-green-100 rounded w-fit">
+            <p className="font-semibold text-lg lg:text-xl">{DisplayPriceInRUpees(data.price)}</p>
+          </div>
+        </div>
+        <button className="my-4 px-4 py-1 border rounded bg-green-600 hover:bg-green-700 text-white">Add</button>
+        <Divider/>
       </div>
     </section>
   );
