b0580b68fe281604e93b5e6dadb7de85bd591d56
Nitin Bharti <jacksp198@gmail.com>
2025-04-05 09:26:59 +0530

    Email

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -1,66 +1,28 @@
-// import { Resend } from "resend";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// if (!process.env.RESEND_API) {
-//   console.log("Please provide RESEND_API inside the .env file.");
-// }
-
-// const resend = new Resend(process.env.RESEND_API);
-
-// const sendEmail = async ({ sendTo, subject, html }) => {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: "Quicko <onboarding@resend.dev>",
-//       to: sendTo,
-//       subject: subject,
-//       html: html,
-//     });
-//     if (error) {
-//       return console.error({ error });
-//     }
-
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default sendEmail;
-
-
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.BREVO_SMTP_KEY) {
-  console.log("Please provide BREVO_SMTP_KEY inside the .env file.");
+if (!process.env.RESEND_API) {
+  console.log("Please provide RESEND_API inside the .env file.");
 }
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "nitinbharti6394358223@gmail.com", 
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-});
+const resend = new Resend(process.env.RESEND_API);
 
 const sendEmail = async ({ sendTo, subject, html }) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Quicko" <nitinbharti6394358223@gmail.com>`,
+    const { data, error } = await resend.emails.send({
+      from: "Quicko <nitinbharti6394358223@gmail.com>",
       to: sendTo,
       subject: subject,
       html: html,
     });
+    if (error) {
+      return console.error({ error });
+    }
 
-    console.log("Email sent:", info.messageId);
-    return info;
+    return data;
   } catch (error) {
-    console.error("Error sending email:", error);
-    throw error;
+    console.log(error);
   }
 };
 
