23a0f999e46a8d2524dbc1e8fd926c455fd5bef6
Nitin Bharti <jacksp198@gmail.com>
2025-05-08 14:56:57 +0530

    AddAwait

--- Frontend/src/pages/Success.jsx
+++ Frontend/src/pages/Success.jsx
@@ -26,11 +26,11 @@ const Success = () => {
       try {
         await Axios(SummaryApi.clearCart);
         dispatch(addToCart([]));
-        // await fetchCartItems();
-        setTimeout(() => {
-          fetchCartItems();
-        }, 500);
-        
+        fetchCartItems();
+        // setTimeout(() => {
+        //   fetchCartItems();
+        // }, 500);
+
         setCartCleared(true);
         return true;
       } catch (error) {
